b45a38c2d31327df984300ec51963f3ebcbf317b
Ian Budden <ian@elucidat.com>
2016-06-13 15:07:16 +0100

    Docs tweak

--- CodeIgniter-3.0.0/application/views/pages_view.php
+++ CodeIgniter-3.0.0/application/views/pages_view.php
@@ -29,7 +29,7 @@
 	  			<ul>
 		  			<li>We've included some useful libraries to get you going. Feel free to use these, or use your own. Up to you.</li>
 		  			<li>The data represents a set of pages. The Learner will progress through the pages in order, following the links within the pages.</li>
-		  			<li>There are often multiple links within a page, so the Learner may not progress in a linear order.</li>
+		  			<li>There are often multiple links within a page, so the Learner may not progress in a linear order - these are shown in the 'other links' variables - and (hint) are the most interesting bit of this test.</li>
 		  			<li>This display is intended for the author of the course, who is wanting to understand how a Learner might progress through the course.</li>
 		  			<li>You must display the structure in an interesting way.</li>
 		  			<li>Feel free to add to the data if you want - if you think an extra bit of page data would be useful to a content author - add it. Extra data could be related to the editing process, or to how Learners are interacting with the page.</li>
@@ -37,6 +37,7 @@
 		  			<li>You must make it interactive (for example implement a filter).</li>
 		  			<li>Please make sure that your filter is implemented server-side (to force you to demonstrate your PHP).</li>
 		  			<li>Documentation is important. So are Unit tests.</li>
+		  			<li>Depending on your platform, there may be some problems running this code out of the box. If that stops you - this probably isn't going to work out...</li>
 	  			</ul>
 	  		</div>
 
@@ -52,7 +53,7 @@
 		<script src="http://canjs.com/release/2.2.4/can.jquery.js"></script>
 		<!-- D3 is a brilliant library for data visualisations -->
 		<script src="http://d3js.org/d3.v3.min.js"></script>
-
+		<!-- Now over to you - have fun! -->
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
 
